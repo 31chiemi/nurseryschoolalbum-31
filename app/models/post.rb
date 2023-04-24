fa44d6d1c516b1086e051d10fddfbc79e0269a5d
31chiemi <31chiemi@gmail.com>
2023-04-24 17:29:05 +0900

    複数投稿可能なバリデーション実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  has_one_attached :image
+  has_many_attached :images
+  validates :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
 end
