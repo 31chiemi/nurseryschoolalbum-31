1a7902d76e2aad09bc18924549974baaeec5c42e
31chiemi <31chiemi@gmail.com>
2023-04-26 18:54:58 +0900

    一覧画面再掲

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    #@posts = Post.all.includes(:user).order("created_at DESC")
+    @posts = Post.all.includes(:user).order("created_at DESC")
   end
   
 
